ba5df9decd4b208f82db4eeb97d9d012c0f359e7
Stella1108 <stellaross2002@gmail.com>
2025-11-28 17:58:21 +0530

    modifying files

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,3 +1,5 @@
+'use client'; // Add this line at the very top
+
 import * as React from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
